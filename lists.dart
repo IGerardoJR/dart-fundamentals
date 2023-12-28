6b9be8b530fdb00a30f0c3c2062af35f04647a11
Isaias <igerardo0808@gmail.com>
2023-12-28 13:38:31 -0600

    functions 1

--- lists.dart
+++ lists.dart
@@ -10,4 +10,15 @@ void main(List<String> args) {
 
   var nombre = "Isaias";
   print(nombre.substring(2));
+
+  var number4 = num.parse('12.56');
+  print('12.56' == number4);
+  print(number4 + 1);
+  print(number4.isFinite);
+  int entero = 3;
+  int negativo = -100;
+  print(number4.sign);
+  print(entero.sign);
+  print(negativo.sign); // Ve si un numero es negativo, positivo o neutro. Devuelve con un decimal si es un double
+  print(negativo.abs()); // Convierte valores negfativos a positivos.
 }
