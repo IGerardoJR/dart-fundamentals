3412c7d3e9ab7ef20e943d4292fe3233a43e1c1a
Isaias <igerardo0808@gmail.com>
2023-12-28 13:47:07 -0600

    functions 2

--- functions.dart
+++ functions.dart
@@ -1,1 +1,20 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  // printToConsole; Sin los () no ejecuta la funcion, solo reserva la memoria.
+  printGreetings('Juan');
+  printNames('Lola');
+  printNames('Hussain', extraString: "Juan");
+  print(multiply(10, 20));
+}
+
+void printGreetings(String name) {
+  print('Hello $name');
+}
+
+void printNames(String string1, {String? extraString}) {
+  print(string1);
+  print(extraString);
+}
+
+int multiply(int1, int int2) {
+  return int1 * int2;
+}
